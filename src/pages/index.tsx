fa2b6aa06f41b482d9ef52cf6aa4b7dfa795e867
Volha Ivanova <Volha.Ivanova@ventionteams.com>
2025-08-27 12:59:05 +0200

    Add Home page with basic SWAPI character list and remove app router

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { Person } from '@/types/Person';
+import CharacterCard from '@/components/CharacterCard';
+
+export default function Home() {
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    fetch('https://swapi.py4e.com/api/people/')
+      .then((res) => res.json())
+      .then((data) => setPeople(data.results));
+  }, []);
+
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">AllianceBook</h1>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {people.map((p) => {
+          const idMatch = p.url.match(/people\/(\d+)\//);
+          const id = idMatch ? idMatch[1] : '';
+          return <CharacterCard key={p.url} person={p} id={id} />;
+        })}
+      </ul>
+    </main>
+  );
+}
