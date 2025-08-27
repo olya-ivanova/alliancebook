45d6434f9e5428f0fa1fe80d8831265019089a56
Volha Ivanova <Volha.Ivanova@ventionteams.com>
2025-08-27 14:36:51 +0200

    Add config files

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['vieraboschkova.github.io'],
+  },
+};
+
+module.exports = nextConfig;
